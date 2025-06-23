8838dbcfc17055ac4c39c8c52353a8fae5f7b0a0
RDePhillip <943336+rdephillip@users.noreply.github.com>
2025-06-23 17:19:31 -0400

    updated project to send to a new tab/page

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -41,6 +41,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
+                  target="_blank"
                   href="https://github.com/rdephillip/eldisc"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
