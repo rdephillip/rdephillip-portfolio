d53e274ba1ae67bc88c5e6ed8a0b9c44a37686bc
RDePhillip <943336+rdephillip@users.noreply.github.com>
2025-06-25 08:09:51 -0400

    restored navbar bg to dark gray

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
   }, [menuOpen]);
 
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(126,42,12,0.2)] backdrop-blur-lg border-b border-orange-50/10 shadow-lg">
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(15,15,15,0.8)] backdrop-blur-lg border-b border-orange-50/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
