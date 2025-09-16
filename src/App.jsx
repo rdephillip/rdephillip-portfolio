0d518b68e47b6c3c0a272858ec494913a8e44f99
RDePhillip <943336+rdephillip@users.noreply.github.com>
2025-09-16 23:24:15 +0000

    Removes Contact Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
-import { Contact } from "./components/sections/Contact";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -26,7 +25,6 @@ function App() {
         <Home />
         <About />
         <Projects />
-        <Contact />
       </div>
     </>
   );
