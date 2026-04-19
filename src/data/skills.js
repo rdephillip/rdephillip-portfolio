4281fe8c11518002d2e396dd90bda1b94eb3b340
RDePhillip <943336+rdephillip@users.noreply.github.com>
2026-04-18 20:45:59 -0400

    feat(skills): establishes the skills data file

---
+++ src/data/skills.js
@@ -0,0 +1,26 @@
+export const skills = [
+  {
+    category: "Languages",
+    items: ["C#", "Go", "Python", "JavaScript", "PowerShell", "SQL", "Batch"]
+  },
+  {
+    category: "Frameworks & Libraries",
+    items: [".NET Core", "ASP.NET", "React", "TailwindCSS"]
+  },
+  {
+    category: "Data",
+    items: ["MS SQL Server", "SQLite", "PostgreSQL"]
+  },
+  {
+    category: "Infrastructure & DevOps",
+    items: ["Docker", "CI/CD Pipelines", "GitHub", "Azure DevOps"]
+  },
+  {
+    category: "Cloud",
+    items: ["Microsoft Azure", "AWS"]
+  },
+  {
+    category: "Architecture & Protocols",
+    items: ["REST APIs", "Microservices", "SAML", "OAuth2", "SFTP"]
+  }
+]
