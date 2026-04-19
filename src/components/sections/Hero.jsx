15ddc8665c4cde9c20f08030944414f5131e9f53
RDePhillip <943336+rdephillip@users.noreply.github.com>
2026-04-18 22:08:38 -0400

    feat(hero): working through designs of hero

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import profile from '../../assets/profile.png'
 
 export const Hero = ({ heroExpanded, setHeroExpanded }) => {
     useEffect(() => {
@@ -13,5 +14,23 @@ export const Hero = ({ heroExpanded, setHeroExpanded }) => {
             window.removeEventListener('wheel', handleScroll)
         }
     }, [])
-    return <section>hero</section>
+    return (
+        <section className={`hero ${heroExpanded ? 'hero--expanded': 'hero--contracted'}`}>
+            <div className="hero__image">
+                <img src={profile} alt="Roy DePhillip" />
+            </div>
+            <div className="hero__content">
+                <h1 className="hero__name">Roy DePhillip</h1>
+                <p className="hero__tagline">Engineers do something the hard way only once</p>
+                <a
+                    href="https://www.linkedin.com/in/rdephillip"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="hero__contact"
+                >
+                    Connect on LinkedIn
+                </a>
+            </div>
+        </section>
+    )
 }
