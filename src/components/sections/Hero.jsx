440c5be6693c275651f4d304c5c5950db5606639
RDePhillip <943336+rdephillip@users.noreply.github.com>
2026-04-18 21:37:55 -0400

    feat(hero): establish baseline hero framework

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,3 +1,17 @@
-export const Hero = () => {
+import { useEffect } from "react"
+
+export const Hero = ({ heroExpanded, setHeroExpanded }) => {
+    useEffect(() => {
+        const handleScroll = () => {
+            setHeroExpanded(false)
+            window.removeEventListener('wheel', handleScroll)
+        }
+
+        window.addEventListener('wheel', handleScroll)
+
+        return () => {
+            window.removeEventListener('wheel', handleScroll)
+        }
+    }, [])
     return <section>hero</section>
 }
