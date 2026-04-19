e70da943a608a7a3b94f373c50b7e76591a9ed5b
RDePhillip <943336+rdephillip@users.noreply.github.com>
2026-04-18 21:05:09 -0400

    feat(data): establishes learning data

---
+++ src/data/learning.js
@@ -0,0 +1,5 @@
+export const learning = {
+  reading: ["Learning Go", "Concurrency in Go", "100 Go Mistakes"],
+  building: ["Meridian", "WithIn"],
+  exploring: ["Rust", "gRPC", "Distributed Systems"]
+}
