789d337201ecbac029f2cf105e3d8f79908efa5a
RDePhillip <943336+rdephillip@users.noreply.github.com>
2026-04-20 21:00:30 -0400

    feat(edu): established educational panel

--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -1,3 +1,16 @@
+import { education } from '../../data'
+import { EducationItem } from '../EducationItem'
+
 export const Education = () => {
-    return <section className="education">education</section>
+    return (
+        <div className='education'>
+            <h2 className='education__title'>Education</h2>
+            {education.map(item => (
+                <EducationItem
+                    key={item.id}
+                    item={item}
+                />
+            ))}
+        </div>
+    )
 }
