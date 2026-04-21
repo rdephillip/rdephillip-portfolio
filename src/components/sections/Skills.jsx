ce06d43d1ef087dcb5da09336cd9a4b3739f7837
RDePhillip <943336+rdephillip@users.noreply.github.com>
2026-04-20 20:27:16 -0400

    feat(skills): established skills panel

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,3 +1,16 @@
+import { skills } from '../../data'
+import { SkillCategory } from '../SkillCategory'
+
 export const Skills = () => {
-    return <section>skills</section>
+    return (
+        <div className='skills__list'>
+            <h2 className='skills__title'>Technical Skills</h2>
+            {skills.map(skill => (
+                <SkillCategory
+                    key={skill.category}
+                    skill={skill}
+                />
+            ))}
+        </div>
+    )
 }
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,26 +1,27 @@
 export const skills = [
   {
     category: "Languages",
+    type: "Language",
     items: ["C#", "Go", "Python", "JavaScript", "PowerShell", "SQL", "Batch"]
   },
   {
     category: "Frameworks & Libraries",
+    type: "Framework",
     items: [".NET Core", "ASP.NET", "React", "TailwindCSS"]
   },
   {
     category: "Data",
+    type: "Data",
     items: ["MS SQL Server", "SQLite", "PostgreSQL"]
   },
   {
     category: "Infrastructure & DevOps",
-    items: ["Docker", "CI/CD Pipelines", "GitHub", "Azure DevOps"]
-  },
-  {
-    category: "Cloud",
-    items: ["Microsoft Azure", "AWS"]
+    type: "Infra",
+    items: ["Docker", "Kubernetes", "CI/CD Pipelines", "GitHub", "Azure DevOps", "Microsoft Azure", "AWS"]
   },
   {
     category: "Architecture & Protocols",
+    type: "Protocol",
     items: ["REST APIs", "Microservices", "SAML", "OAuth2", "SFTP"]
   }
 ]
