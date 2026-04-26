d20967eb84b9bd452b1e77ce3d3b75ccfd35ca0e
RDePhillip <943336+rdephillip@users.noreply.github.com>
2026-04-26 14:37:47 -0400

    feat: implements experience and styling

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -1,3 +1,18 @@
+import { experience } from "../../data"
+import { ExperienceItem } from "../ExperienceItem"
+
 export const Experience = () => {
-    return <section className="experience">experience</section>
+    return (
+        <div className="experience">
+            <h2 className="experience__title">Experience</h2>
+            <div className="experience__container">
+                {experience.map(item => (
+                    <ExperienceItem
+                        key={item.id}
+                        item={item}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
